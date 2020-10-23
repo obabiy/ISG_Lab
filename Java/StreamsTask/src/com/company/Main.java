5409c127c75497f6eb0120a7859e5edd9b9896ed
obabiy <alex.babiy.sq@gmail.com>
2020-10-23 12:06:55 +0300

    Fibonacci series

--- Java/StreamsTask/src/com/company/Main.java
+++ Java/StreamsTask/src/com/company/Main.java
@@ -1,11 +1,15 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
         evenNumbersStream(0, 100);      // even number stream | seed - first number; if limit = 0 -> infinity loop
+        fibonacciStream(50);                // fibonacci series with limit numbers
     }
 
     public static void evenNumbersStream(int seed, int limit){
@@ -14,7 +18,9 @@ public class Main {
         else inputStream.filter(i -> i % 2 == 0).forEach(System.out::println);      // no limit -> infinity stream
     }
 
-    public static void fibonacciStream(int limitNums){
+    public static void fibonacciStream(long limitNums){
+        long[] arrFib = {1, 1};
 
+        Stream.iterate(arrFib, a -> a = new long[]{a[1], a[0] + a[1]}).limit(limitNums).forEach(a -> System.out.println(a[0]));
     }
 }
